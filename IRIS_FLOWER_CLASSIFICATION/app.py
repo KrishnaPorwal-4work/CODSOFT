90890d99897925b665f4ad8f5d235728a9dbe65f
KRISHNA PORWAL <krishnaporwal4work@gmail.com>
2026-04-08 21:27:39 +0530

    Added Streamlit UI

---
+++ IRIS_FLOWER_CLASSIFICATION/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+# Load model
+with open("Iris_Flower_Classification.pkl", "rb") as f:
+    model = pickle.load(f)
+
+st.title("Iris Flower Prediction")
+
+# Inputs
+sepal_length = st.slider("Sepal Length (cm)", 4.0, 8.0)
+sepal_width = st.slider("Sepal Width (cm)", 1.5, 5.0)
+petal_length = st.slider("Petal Length (cm)", 1.0, 8.0)
+petal_width = st.slider("Petal Width (cm)", 0.0, 3.0)
+
+# Prediction
+if st.button("Predict"):
+    input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction  = model.predict(input_data)
+
+    species = ["Setosa", "Versicolor", "Virginica"]
+
+    st.success(f"Predicted Iris Species: {species[prediction[0]]}")
